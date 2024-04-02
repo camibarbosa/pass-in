31539f7cb6da0badc454f761bbca7900e6c6623f
Camila <97048604+camibarbosa@users.noreply.github.com>
2024-04-02 17:46:10 -0300

    refactor: atualizando entidades

--- pass-in/src/main/java/br/com/camila/barbosa/passin/domain/events/Event.java
+++ pass-in/src/main/java/br/com/camila/barbosa/passin/domain/events/Event.java
@@ -12,18 +12,21 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Events {
+public class Event {
     @Id
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String details;
+
     @Column(nullable = false, unique = true)
     private String slug;
-    @Column(nullable = false, name = "maximum_attendees")
+
+    @Column(nullable = false, name="maximum_attendees")
     private Integer maximumAttendees;
 }
-
