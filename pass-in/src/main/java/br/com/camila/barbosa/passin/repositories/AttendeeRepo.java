451daaf35f8a64b044762f17cc08a3f141e56ba5
Camila <97048604+camibarbosa@users.noreply.github.com>
2024-04-03 15:45:35 -0300

    implementação dos repositorios

--- pass-in/src/main/java/br/com/camila/barbosa/passin/repositories/AttendeeRepo.java
+++ pass-in/src/main/java/br/com/camila/barbosa/passin/repositories/AttendeeRepo.java
@@ -2,8 +2,9 @@ package br.com.camila.barbosa.passin.repositories;
 
 import br.com.camila.barbosa.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AttendeeRepo extends JpaRepository<Attendee, String> {
+import java.util.List;
+
+public interface AttendeeRepo extends JpaRepository<Attendee, String>{
+    List<Attendee> findByEventsId(String eventId);
 }
--- pass-in/src/main/java/br/com/camila/barbosa/passin/repositories/EventRepo.java
+++ pass-in/src/main/java/br/com/camila/barbosa/passin/repositories/EventRepo.java
@@ -1,9 +1,9 @@
 package br.com.camila.barbosa.passin.repositories;
 
-import br.com.camila.barbosa.passin.domain.events.Events;
+import br.com.camila.barbosa.passin.domain.events.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepo extends JpaRepository<Events, String> {
+public interface EventRepo extends JpaRepository<Event, String> {
 }
